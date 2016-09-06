2c16e6922ec50e214bf05a9dbaad494aa677cbfa
Janak Bhalla <janak1710@gmail.com>
2016-09-05 23:53:37 -0400

    67, 2, 168 added

---
+++ Java/AddTwoNumbers.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		return addUtils(l1, l2, 0);
+	}
+
+	private ListNode addUtils(ListNode l1, ListNode l2, int carry) {
+		if (l1 == null && l2 == null && carry == 0)
+			return null;
+		int sum = carry;
+		if (l1 != null)
+			sum += l1.val;
+		if (l2 != null)
+			sum += l2.val;
+		ListNode head = new ListNode(sum % 10);
+		head.next = addUtils(l1 == null ? null : l1.next, l2 == null ? null
+				: l2.next, sum / 10);
+		return head;
+	}
+}
---
+++ Java/ExcelSheetColumnTitle.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public String convertToTitle(int n) {
+        StringBuffer sb = new StringBuffer();
+        while(n > 0){
+            n--;
+            sb.append((char)(n%26 + 'A'));
+            n /= 26;
+        }
+        return sb.reverse().toString();
+    }
+}
